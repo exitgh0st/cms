318385f255d866179dd58a5f470ea078ecb53f07
Adonis Alcantara <exitghostnull@gmail.com>
2023-05-23 19:37:10 +0800

    Add student login page

---
+++ src/app/pages/student-login/student-login.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Student } from 'src/app/models/student';
+import { StudentAuthService } from 'src/app/services/student-auth.service';
+import { first } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-student-login',
+  templateUrl: './student-login.component.html',
+  styleUrls: ['./student-login.component.scss']
+})
+export class StudentLoginComponent {
+  loading: boolean = false;
+
+  studentForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('')
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private studentAuthService: StudentAuthService
+  ) {}
+
+  login() {
+    if (this.studentForm.invalid) {
+      return;
+    }
+
+    const student: Student = this.studentForm.value;
+
+    if (!student.email || student.password == undefined) {
+      console.log('NULL');
+
+      return;
+    }
+
+    console.log('LOGIN');
+    this.loading = true;
+
+    this.studentAuthService
+      .login(student.email, student.password)
+      .pipe(first())
+      .subscribe((response) => {
+        const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/student/dashboard';
+        this.router.navigateByUrl(returnUrl);
+      });
+  }
+
+  getEmailErrorMessage() {
+    if (this.studentForm.get('email')?.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.studentForm.get('email')?.hasError('email') ? 'Not a valid email' : '';
+  }
+}
