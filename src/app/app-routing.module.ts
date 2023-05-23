1511ea3980138f96adb79529a807b157d081fd47
Adonis Alcantara <exitghostnull@gmail.com>
2023-05-23 19:38:40 +0800

    Update app module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
+import { StudentLoginComponent } from './pages/student-login/student-login.component';
+import { StudentDashboardComponent } from './pages/student-dashboard/student-dashboard.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { StudentDepartmentRequirementComponent } from './pages/student-department-requirement/student-department-requirement.component';
+import { StudentProfileComponent } from './pages/student-profile/student-profile.component';
+import { SuperAdminDashboardComponent } from './pages/super-admin-dashboard/super-admin-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login/student', component: StudentLoginComponent },
+  { path: 'login/admin', component: AdminLoginComponent },
+  { path: 'login', component: LandingPageComponent },
+  { path: 'student/dashboard', component: StudentDashboardComponent },
+  { path: 'admin/dashboard', component: AdminDashboardComponent },
+  { path: 'super-admin/dashboard', component: SuperAdminDashboardComponent },
+  {
+    path: 'student/deparment/:department_id',
+    component: StudentDepartmentRequirementComponent,
+  },
+  {
+    path: 'student/profile',
+    component: StudentProfileComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
+import { StudentLoginComponent } from './pages/student-login/student-login.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { SuperAdminDashboardComponent } from './pages/super-admin-dashboard/super-admin-dashboard.component';
+import { StudentDashboardComponent } from './pages/student-dashboard/student-dashboard.component';
+import { StudentDepartmentRequirementComponent } from './pages/student-department-requirement/student-department-requirement.component';
+import { StudentProfileComponent } from './pages/student-profile/student-profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ErrorInterceptor } from './interceptors/error-interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    AdminLoginComponent,
+    StudentLoginComponent,
+    AdminDashboardComponent,
+    SuperAdminDashboardComponent,
+    StudentDashboardComponent,
+    StudentDepartmentRequirementComponent,
+    StudentProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
