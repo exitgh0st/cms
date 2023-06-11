cdca97e87017246cb3465292fb4d70cf8251e331
Adonis Alcantara <exitghostnull@gmail.com>
2023-06-12 07:26:05 +0800

    Add roleguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,6 @@ import { AdminStudentsComponent } from './pages/admin-students/admin-students.co
 import { AdminRequirementsComponent } from './pages/admin-requirements/admin-requirements.component';
 import { AuthGuard } from './guards/auth.guard';
 import { LoginGuard } from './guards/login.guard';
-import { ErikaComponent } from './pages/erika/erika.component';
 import { StudentListComponent } from './pages/student-list/student-list.component';
 import { SuperAdminStudentsComponent } from './pages/super-admin-students/super-admin-students.component';
 import { SuperAdminAdminsComponent } from './pages/super-admin-admins/super-admin-admins.component';
@@ -26,85 +25,181 @@ import { SuperAdminUsersManualComponent } from './pages/super-admin-users-manual
 import { AdminFaqComponent } from './pages/admin-faq/admin-faq.component';
 import { AdminUsersManualComponent } from './pages/admin-users-manual/admin-users-manual.component';
 import { AdminStudentRequirementsComponent } from './pages/admin-student-requirements/admin-student-requirements.component';
+import { RoleGuard } from './guards/role.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LandingPageComponent, canActivate: [LoginGuard] },
   { path: 'login/:loginType', component: LoginComponent, canActivate: [LoginGuard] },
-  { path: 'student/dashboard', component: StudentDashboardComponent, canActivate: [AuthGuard] },
-  { path: 'admin/dashboard', component: AdminDashboardComponent, canActivate: [AuthGuard] },
-  { path: 'admin/students', component: AdminStudentsComponent, canActivate: [AuthGuard] },
-  { path: 'admin/requirements/:departmentId', component: AdminRequirementsComponent, canActivate: [AuthGuard] },
+  {
+    path: 'student/dashboard',
+    component: StudentDashboardComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'student'
+    }
+  },
+  {
+    path: 'admin/dashboard',
+    component: AdminDashboardComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'clearance_admin'
+    }
+  },
+  {
+    path: 'admin/students',
+    component: AdminStudentsComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'clearance_admin'
+    }
+  },
+  {
+    path: 'admin/requirements/:departmentId',
+    component: AdminRequirementsComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'clearance_admin'
+    }
+  },
   {
     path: 'admin/profile',
     component: AdminProfileComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'clearance_admin'
+    }
   },
-  { path: 'admin/student-list', component: StudentListComponent },
+  {
+    path: 'admin/student-list',
+    component: StudentListComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'clearance_admin'
+    }
+  },
   {
     path: 'admin/faqs',
     component: AdminFaqComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'clearance_admin'
+    }
   },
   {
     path: 'admin/users-manual',
     component: AdminUsersManualComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'clearance_admin'
+    }
   },
 
   {
     path: 'admin/students/:studentNumber',
     component: AdminStudentRequirementsComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'clearance_admin'
+    }
   },
-  { path: 'super-admin/dashboard', component: SuperAdminDashboardComponent, canActivate: [AuthGuard] },
-  { path: 'super-admin/clearance', component: SuperAdminClearanceComponent, canActivate: [AuthGuard] },
+  {
+    path: 'super-admin/dashboard',
+    component: SuperAdminDashboardComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'super_admin'
+    }
+  },
+  {
+    path: 'super-admin/clearance',
+    component: SuperAdminClearanceComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'super_admin'
+    }
+  },
 
-  { path: 'super-admin/students', component: SuperAdminStudentsComponent, canActivate: [AuthGuard] },
-  { path: 'super-admin/admins', component: SuperAdminAdminsComponent, canActivate: [AuthGuard] },
+  {
+    path: 'super-admin/students',
+    component: SuperAdminStudentsComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'super_admin'
+    }
+  },
+  {
+    path: 'super-admin/admins',
+    component: SuperAdminAdminsComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'super_admin'
+    }
+  },
   {
     path: 'super-admin/faqs',
     component: SuperAdminFaqComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'super_admin'
+    }
   },
   {
     path: 'super-admin/users-manual',
     component: SuperAdminUsersManualComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'super_admin'
+    }
   },
   {
     path: 'student/requirements/:departmentId',
     component: StudentDepartmentRequirementComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'student'
+    }
   },
   {
     path: 'student/profile',
     component: StudentProfileComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'student'
+    }
   },
   {
     path: 'student/clearance',
     component: StudentClearanceComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'student'
+    }
   },
   {
     path: 'student/contact',
     component: ContactPageComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'student'
+    }
   },
   {
     path: 'student/faqs',
     component: StudentFaqComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'student'
+    }
   },
   {
     path: 'student/users-manual',
     component: StudentUsersManualComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'erika',
-    component: ErikaComponent
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      role: 'student'
+    }
   }
 ];
 
